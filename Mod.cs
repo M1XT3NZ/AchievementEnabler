24082552e6f58e6616df9be155a0c2737beafabf
Jacob Rice <7104854+Wayzware@users.noreply.github.com>
2024-03-26 00:05:15 -0500

    initial version

---
+++ Mod.cs
@@ -0,0 +1,40 @@
+﻿using Colossal.Logging;
+using Colossal.Serialization.Entities;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using Unity.Entities;
+
+namespace AchievementEnabler
+{
+    public class Mod : IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(AchievementEnabler)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            World.DefaultGameObjectInjectionWorld.CreateSystem<UnlockerSystem>();
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+        }
+    }
+
+    public partial class UnlockerSystem : GameSystemBase
+    {
+        protected override void OnGameLoadingComplete(Purpose purpose, GameMode mode)
+        {
+            Colossal.PSI.Common.PlatformManager.instance.achievementsEnabled = true;
+        }
+
+        protected override void OnUpdate() 
+        { }
+    }
+}
