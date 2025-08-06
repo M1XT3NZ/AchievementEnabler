efd34ad8dd80286cfb2058e2e69fb44ff072a378
Michael <sven.maier15@icloud.com>
2025-08-07 01:50:51 +0200

    Fixed the Mod

--- Mod.cs
+++ Mod.cs
@@ -1,4 +1,5 @@
 ﻿using Colossal.Logging;
+using Colossal.PSI.Common;
 using Colossal.Serialization.Entities;
 using Game;
 using Game.Modding;
@@ -9,21 +10,21 @@ namespace AchievementEnabler
 {
     public class Mod : IMod
     {
-        public static ILog log = LogManager.GetLogger($"{nameof(AchievementEnabler)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        public static ILog _log = LogManager.GetLogger($"{nameof(AchievementEnabler)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
 
         public void OnLoad(UpdateSystem updateSystem)
         {
-            log.Info(nameof(OnLoad));
+            _log.Info(nameof(OnLoad));
 
             if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
-                log.Info($"Current mod asset at {asset.path}");
+                _log.Info($"Current mod asset at {asset.path}");
 
             World.DefaultGameObjectInjectionWorld.CreateSystem<UnlockerSystem>();
         }
 
         public void OnDispose()
         {
-            log.Info(nameof(OnDispose));
+            _log.Info(nameof(OnDispose));
         }
     }
 
@@ -31,7 +32,14 @@ namespace AchievementEnabler
     {
         protected override void OnGameLoadingComplete(Purpose purpose, GameMode mode)
         {
-            Colossal.PSI.Common.PlatformManager.instance.achievementsEnabled = true;
+            PlatformManager.instance.achievementsEnabled = true;
+            PlatformManager.instance.onAchievementUpdated += (backend, id) =>
+            {
+                Mod._log.Info($"Achievement updated: {id}");
+                PlatformManager.instance.GetAchievement(id, out var achi);
+                PlatformManager.instance.IndicateAchievementProgress(id,achi.progress);
+            };
+            Mod._log.Info("Achievements enabled");
         }
 
         protected override void OnUpdate() 
